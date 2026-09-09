2dc6b176a13a2eca6a1b975055e5241f680d0197
Miiduoa <107848266+Miiduoa@users.noreply.github.com>
2026-09-09 15:18:20 +0800

    Load fixed auth flow before app modules

--- pu-plan/boot-v9.js
+++ pu-plan/boot-v9.js
@@ -11,6 +11,7 @@ link('stylesheet',asset('./mobile-v12.css'),'puplanMobileV12');
 link('manifest',asset('./manifest-v9.webmanifest'),'puplanManifestV9');
 link('apple-touch-icon',asset('./icon.svg'),'puplanIconV9');
 for(const [name,content] of [['apple-mobile-web-app-capable','yes'],['apple-mobile-web-app-status-bar-style','default'],['apple-mobile-web-app-title','PU/PLAN']]){if(!document.querySelector(`meta[name="${name}"]`)){const m=document.createElement('meta');m.name=name;m.content=content;document.head.appendChild(m)}}
+await import(asset('./auth-v13.js'));
 await import(asset('./next-round.js'));
 await import(asset('./polish-v9.js'));
 await import(asset('./social-router-v12.js'));
