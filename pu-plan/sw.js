8f9f92e00c369927b9ea19d1ce75c09b1def8564
Miiduoa <107848266+Miiduoa@users.noreply.github.com>
2026-09-10 14:46:19 +0800

    Invalidate old mobile shell cache

--- pu-plan/sw.js
+++ pu-plan/sw.js
@@ -1,4 +1,4 @@
-const CACHE='nolu-shell-20260910-1405';
+const CACHE='nolu-shell-20260910-1435';
 const ROOT=new URL('./',self.registration.scope).href;
 const SHELL=['./','./index.html','./manifest.webmanifest','./nolu-icon.svg'].map(path=>new URL(path,ROOT).href);
 
