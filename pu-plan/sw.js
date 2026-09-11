fa4e23ee21ee72fafb7937c0b2bd09bf76e347d9
Miiduoa <107848266+Miiduoa@users.noreply.github.com>
2026-09-11 13:49:16 +0800

    chore(nolu): keep existing shell cache contract

--- pu-plan/sw.js
+++ pu-plan/sw.js
@@ -1,4 +1,4 @@
-const CACHE='nolu-shell-20260911-startup-trust1';
+const CACHE='nolu-shell-20260911-outage-mesh5';
 const ROOT=new URL('./',self.registration.scope).href;
 const PATHS=[
   './','./index.html','./manifest.webmanifest','./nolu-icon.svg','./nolu-mesh-jwks.json',
