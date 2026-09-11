0a656c52c4fbbf54e45ce993b8b34f7f14d9786b
Miiduoa <107848266+Miiduoa@users.noreply.github.com>
2026-09-11 13:00:12 +0800

    chore(nolu): roll mesh4 shell for device rescue v2

--- pu-plan/sw.js
+++ pu-plan/sw.js
@@ -1,4 +1,4 @@
-const CACHE='nolu-shell-20260911-device-rescue1-mesh4';
+const CACHE='nolu-shell-20260911-device-rescue2-mesh4';
 const ROOT=new URL('./',self.registration.scope).href;
 const PATHS=[
   './','./index.html','./manifest.webmanifest','./nolu-icon.svg','./nolu-mesh-jwks.json',
